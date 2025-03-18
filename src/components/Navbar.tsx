378a1dc83b8c8c203a15a7728d2d954f480a04cf
Beau Yancey <yancey.beau.j@gmail.com>
2025-03-17 18:35:26 -0600

    add navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import MenuIcon from "../assets/menu.svg?react"
+
+export default function Navbar() {
+	const [navbarState, setNavbarState] = useState('open')
+	return (
+		<>
+			<nav className={navbarState}>
+				<button id='nav-toggle' className={`nav-${navbarState}`} onClick={() => setNavbarState(prev => prev === 'closed' ? 'open' : 'closed')}>
+					<MenuIcon />
+				</button>
+				<div className='nav-content'>
+					<a href='/'>Quiz</a>
+					<a href='/'>Learn More</a>
+					<a href='/'>Commit to Growth</a>
+				</div>
+			</nav>
+		</>
+	);
+}
